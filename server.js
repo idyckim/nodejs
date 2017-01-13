a5bf8dd3c06231ab790d7c6c2284f326e91238a7
YoonChul Kim <yckim@geninetworks.com>
2017-01-13 17:38:38 +0900

    test js

---
+++ server.js
@@ -0,0 +1,28 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+//서버생성
+http.createServer( function (request, response) {
+	var pathname = url.parse(request.url).pathname;
+
+	console.log("Request for " + pathname + " received.");
+
+	if (pathname == "/") {
+		pathname = "/index.html";
+	}
+
+	fs.readFile(pathname.substr(1), function (err, data) {
+		if (err) {
+			console.log(err);
+			response.writeHead(404, {'Content-Type':'text/html'});
+		} else {
+			response.writeHead(200, {'Content-Type': 'text/html'});
+			response.write(data.toString());
+		}
+
+		response.end();
+	});
+}).listen(8081);
+
+console.log('Server running at http://localhost:8081');
